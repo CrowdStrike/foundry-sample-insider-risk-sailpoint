e67016f40366917e712a080acdd476c945be98a6
Matt Raible <matt.raible@crowdstrike.com>
2025-12-18 13:16:29 -0700

    Re-enable workflow provisioning and test both workflows

--- e2e/tests/app-install.setup.ts
+++ e2e/tests/app-install.setup.ts
@@ -1,19 +1,15 @@
 import { test, test as setup } from '../src/fixtures';
 
-setup('install SailPoint Insider Risk app', async ({ appCatalogPage, appBuilderPage, appName }) => {
-  // Extend timeout for this test - disabling workflow provisioning + deploy/release takes time
-  test.setTimeout(300000); // 5 minutes
+setup('install SailPoint Insider Risk app', async ({ appCatalogPage, appName }) => {
+  test.setTimeout(120000); // 2 minutes for installation
 
   // Check if app is already installed (this navigates to the app page)
   const isInstalled = await appCatalogPage.isAppInstalled(appName);
 
   if (!isInstalled) {
-    console.log(`App '${appName}' is not installed. Disabling workflow provisioning and installing...`);
+    console.log(`App '${appName}' is not installed. Installing...`);
 
-    // Disable workflow provisioning before installing
-    await appBuilderPage.disableWorkflowProvisioning(appName);
-
-    // Install the app
+    // Install the app (workflows will be provisioned with valid SailPoint credentials)
     const installed = await appCatalogPage.installApp(appName);
 
     if (!installed) {
